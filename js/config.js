8139506d5a7434e2e244824cf1bae874a34e5a4f
Eve <evelisevazquez@gmail.com>
2018-06-28 22:24:31 -0300

    [Work in progress] Initializing - Nav + About me

---
+++ js/config.js
@@ -0,0 +1,32 @@
+angular.module('app', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+	$stateProvider
+
+		.state('menu', {
+			abstract:true,
+            // url: '/home',
+            views: {
+                'menu@': {
+                    templateUrl: '/templates/menu.html',
+                    // controller: 'loginController'
+                }
+            }
+        })
+
+        .state('home', {
+            url: '/home',
+            parent: 'menu',
+            views: {
+                'content@menu': {
+                    templateUrl: '/templates/home.html',
+                    // controller: 'loginController'
+                }
+            }
+        })
+
+
+
+    $urlRouterProvider.otherwise('/home');
+
+});
